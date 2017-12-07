a193d2a307cf17dd32c5cb88577c136b875338ee
Eric Evans <eevans@gogoair.com>
2017-12-07 16:19:40 -0700

    added default from previous scope

--- pytesting/conftest.py
+++ pytesting/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 
-@pytest.fixture(scope='session', params=['defaultuser'])
+@pytest.fixture(scope='module', params=['defaultuser'])
 def username(request):
+    print 'Request:{}'.format(request)
     user = request.param
     print '\nConftest Fixture:{}\n'.format(user)
     return user
--- pytesting/test_fixtures_function.py
+++ pytesting/test_fixtures_function.py
@@ -5,17 +5,21 @@ def setup_module():
 
 
 @pytest.fixture(scope="function")
-def setup_fixture(username):
+def func_username(username):
     user = username
     print "\nIn Setup_fixture with {}.\n".format(user)
     return user
 
-@pytest.mark.parametrize('setup_fixture', ['functionuser1'])
-def test_test1(setup_fixture):
+@pytest.mark.parametrize('func_username', ['functionuser1'])
+def test_test1(func_username):
     print '\nrunning test1\n'
-    print 'Setup fixture returned:{}\n'.format(setup_fixture)
+    print 'Setup fixture returned:{}\n'.format(func_username)
 
-@pytest.mark.parametrize('setup_fixture', ['functionuser2'])
-def test_test2(setup_fixture):
+@pytest.mark.parametrize('username', ['functionuser2'])
+def test_test2(func_username):
     print '\nrunning test2\n'
-    print 'Setup fixture returned:{}\n'.format(setup_fixture)
+    print 'Setup fixture returned:{}\n'.format(func_username)
+
+def test_test3(func_username):
+    print '\nrunning test3\n'
+    print 'Setup fixture returned:{}\n'.format(func_username)
